512166365904e8a9328c7138bc29f30f37e4e02c
hazar gul nazari <nazariazargul@gmail.com>
2023-11-21 12:22:33 +0100

    Change (refactored) to src/main/java/jpa/test/SpringJpaExamplesApplication.java

--- src/main/java/jpa/test/SpringJpaExamplesApplication.java
+++ src/main/java/jpa/test/SpringJpaExamplesApplication.java
@@ -1,13 +1,7 @@
 package jpa.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class SpringJpaExamplesApplication {
@@ -15,13 +9,5 @@ public class SpringJpaExamplesApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringJpaExamplesApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner commandLineRunner(@Autowired BookRepository bookRepository){
-//        return args -> {
-//            System.out.println("Assert is running");
-//            assert !bookRepository.findAll().isEmpty();
-//        };
-//    }
 
 }
